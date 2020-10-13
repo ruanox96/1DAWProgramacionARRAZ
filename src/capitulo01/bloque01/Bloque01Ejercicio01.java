5a74be45bd257790ef0759088a8df85b0641982b
ruanox96 <72544074+ruanox96@users.noreply.github.com>
2020-10-13 12:38:32 +0200

    ddd

--- src/capitulo01/bloque01/Bloque01Ejercicio01.java
+++ src/capitulo01/bloque01/Bloque01Ejercicio01.java
@@ -17,18 +17,6 @@ public class Bloque01Ejercicio01 {
 				"\nEl valor de la variable numDouble es: " + numDouble);
 	
 	}
-	public static void resolucionEcuacionSegundoGrado () {
-		int a = 1, b = 0, c = -1;
-		float x1, x2;
-		float determinante;
-		
-		determinante = (float) Math.sqrt(b*b - 4*a*c);
-		x1 = ((0 - b) + determinante) / (2 * a);
-		x2 = ((0 - b) - determinante) / (2 * a);
-		
-		System.out.println("La ecuación es " + a + "x(2) + " + b + "x + " + c + " = 0");
-		System.out.println("La solución x1 es " + x1 + " y la solución x2 es " + x2);
-		
-	}
+	
 
 }
