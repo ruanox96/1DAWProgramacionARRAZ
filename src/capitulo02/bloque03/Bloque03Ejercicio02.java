378af397c89938b6213dfd9814910001a8b1bc10
ruanox96 <72544074+ruanox96@users.noreply.github.com>
2020-10-19 17:06:34 +0200

    hh

--- src/capitulo02/bloque03/Bloque03Ejercicio02.java
+++ src/capitulo02/bloque03/Bloque03Ejercicio02.java
@@ -1,9 +1,27 @@
 package capitulo02.bloque03;
 
+import javax.swing.JOptionPane;
+
 public class Bloque03Ejercicio02 {
 
 	public static void main(String[] args) {
 		
+		System.out.println("1. Cálculo de la hipotenusa de un triángulo.");
+		System.out.println("2. Cálculo de la superficie de una circunferencia.");
+		System.out.println("3. Cálculo del perímetro de una circunferencia.");
+		System.out.println("4. Cálculo del área de un rectángulo.");
+		System.out.println("5. Cálculo del área de un triángulo.");
+		System.out.println("0. Salir de la aplicación.");
+		
+		String str = JOptionPane.showInputDialog("Escribe el numero 1");
+		int opc = Integer.parseInt(str);
+		
+		switch(opc) {
+		case 1:
+			int num1;
+			num1 = JOptionPane.showInputDialog("Escribe el numero 1");
+			float num1 = Float.parseFloat(str);
+		}
 
 	}
 
