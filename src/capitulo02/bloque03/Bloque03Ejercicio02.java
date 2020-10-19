8d44265caabeb160912e9618a04d269a41fc13e5
Ruanox <Ruanox@192.168.1.132>
2020-10-19 19:31:44 +0200

    sad

--- src/capitulo02/bloque03/Bloque03Ejercicio02.java
+++ src/capitulo02/bloque03/Bloque03Ejercicio02.java
@@ -4,23 +4,84 @@ import javax.swing.JOptionPane;
 
 public class Bloque03Ejercicio02 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws InterruptedException {
+		// Creamos el menu con las opciones disponibles
 		System.out.println("1. Cálculo de la hipotenusa de un triángulo.");
 		System.out.println("2. Cálculo de la superficie de una circunferencia.");
 		System.out.println("3. Cálculo del perímetro de una circunferencia.");
 		System.out.println("4. Cálculo del área de un rectángulo.");
 		System.out.println("5. Cálculo del área de un triángulo.");
 		System.out.println("0. Salir de la aplicación.");
-		
-		String str = JOptionPane.showInputDialog("Escribe el numero 1");
+		System.out.println("                        ");
+		//REcogemos el numero de opcion introducia por el usuario
+		String str = JOptionPane.showInputDialog("Escribe una opción");
 		int opc = Integer.parseInt(str);
+		float num1;
+		float num2;
+		float area;
+		float radio;
+		float hipo;
+		float perimetro;
+		float base;
+		float altura;
+		
 		
 		switch(opc) {
 		case 1:
-			int num1;
-			num1 = JOptionPane.showInputDialog("Escribe el numero 1");
-			float num1 = Float.parseFloat(str);
+			str = JOptionPane.showInputDialog("Escribe el primer cateto");
+			num1 = Float.parseFloat(str);
+			str = JOptionPane.showInputDialog("Escribe el segundo cateto");
+			num2 = Float.parseFloat(str);
+			hipo = (float) Math.sqrt(Math.pow(num1, 2) + Math.pow(num2, 2));
+			System.out.println("La hipotenusa es: " +  hipo);
+			
+			Thread.sleep (4000);
+			break;
+		case 2:
+			str = JOptionPane.showInputDialog("Escribe el radio");
+			radio = Float.parseFloat(str);
+			area = (float) (Math.PI * Math.pow(radio,2));
+			System.out.println("El area de la circunferencia es:" + area);
+
+			Thread.sleep (4000);
+			break;
+		
+		case 3:
+			str = JOptionPane.showInputDialog("Escribe el perimetro");
+			perimetro = Float.parseFloat(str);
+			str = JOptionPane.showInputDialog("Escribe el radio");
+			radio = Float.parseFloat(str);
+			perimetro = (float) (2*Math.PI*radio);
+			System.out.println("El perimetro es: " + perimetro);
+
+			Thread.sleep (4000);
+			break;
+			
+		case 4:
+			str = JOptionPane.showInputDialog("Escribe la base");
+			base = Float.parseFloat(str);
+			str = JOptionPane.showInputDialog("Escribe la altura");
+			altura = Float.parseFloat(str);
+			area = base * altura;
+			System.out.println("El area de un rectangulo con base: " + base + " y con altura: " + altura + " es: " + area);
+
+			Thread.sleep (4000);
+			break;
+			
+		case 5:
+			str = JOptionPane.showInputDialog("Escribe la base");
+			base = Float.parseFloat(str);
+			str = JOptionPane.showInputDialog("Escribe la altura");
+			altura = Float.parseFloat(str);
+			area = (base*altura)/2;
+			System.out.println("El area de un triangulo con base: " + base + " y con altura: " + altura + " es: " + area);
+
+			Thread.sleep (4000);
+			break;
+		
+		case 0:
+			System.out.println("Has introducido el 0, saliendo del programa :)");
+			break;
 		}
 
 	}
