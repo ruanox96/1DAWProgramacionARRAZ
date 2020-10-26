d541a8ebc46322cae3f6ec5139d4a9b56a65778e
ruanox96 <72544074+ruanox96@users.noreply.github.com>
2020-10-26 17:33:54 +0100

    jj

--- src/capitulo03/bloque03/Bloque03Ejercicio06.java
+++ src/capitulo03/bloque03/Bloque03Ejercicio06.java
@@ -9,7 +9,7 @@ public class Bloque03Ejercicio06 {
 		 * Realiza un ejercicio que pida números al usuario, hasta que uno de ellos sea 0 (cero).
 		 *  Para cada número introducido se debe mostrar en pantalla su tabla de multiplicar, desde el 0 (cero) hasta el 10.
 		 */
-		int num=0, i, cont=0;
+		int num=0, i;
 		
 		do{   // Vas a ejecutar las instrucciones del bucle al menos una vez.
 			String str = JOptionPane.showInputDialog("Escribe un numero para ver su tabla de multiplicar");
