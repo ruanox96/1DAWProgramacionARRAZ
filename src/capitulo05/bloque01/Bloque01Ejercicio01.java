0a4ddd3f3df8cb9b03ef5b01ec5e4f1e84da6abd
ruanox96 <72544074+ruanox96@users.noreply.github.com>
2020-10-28 12:32:27 +0100

    bolbo

--- src/capitulo05/bloque01/Bloque01Ejercicio01.java
+++ src/capitulo05/bloque01/Bloque01Ejercicio01.java
@@ -1,10 +1,10 @@
-package bloque01;
+package capitulo05.bloque01;
 
 public class Bloque01Ejercicio01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num=0;
+
 	}
 
 }
