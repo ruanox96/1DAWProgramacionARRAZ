2382ce4e5add81731828c5c5f486a5baaac9c984
ruanox96 <72544074+ruanox96@users.noreply.github.com>
2020-10-28 17:18:08 +0100

    ggg

--- src/capitulo05/bloque01/Bloque01Ejercicio01.java
+++ src/capitulo05/bloque01/Bloque01Ejercicio01.java
@@ -1,9 +1,18 @@
 package capitulo05.bloque01;
 
 public class Bloque01Ejercicio01 {
+	
+	public static int obtenerNumeroAzar () {
+		 return (int) Math.round(Math.random() * 100);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int numeros[] = new int[obtenerNumeroAzar ()];
+		
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = i + 1;
+			System.out.print(numeros[i] + " ");
+		}
 
 	}
 
