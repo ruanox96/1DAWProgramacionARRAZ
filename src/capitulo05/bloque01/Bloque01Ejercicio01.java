d95bc2b38c40a33f2eedefb442e16958f6aa5622
Ruanox <Ruanox@192.168.1.132>
2020-10-28 18:49:15 +0100

    bolbooo

--- src/capitulo05/bloque01/Bloque01Ejercicio01.java
+++ src/capitulo05/bloque01/Bloque01Ejercicio01.java
@@ -7,13 +7,35 @@ public class Bloque01Ejercicio01 {
 	}
 
 	public static void main(String[] args) {
-		int numeros[] = new int[5];
-		
+		/*
+		 * Realiza un ejercicio que inicialice un array de 150 elementos enteros al azar entre 0 y 100.
+		 * Debes mostrar el array en pantalla y obtener la suma, la media, el menor y el mayor de todos los números que aparecen en el array.
+		 */
+		int numeros[] = new int[3];
+		int suma=0, mayor=0, menor=0;
+		float media;
+		//Bucle for con el que recorremos todas las posiciones del array, el length es para que pare cuando se acabe el array
 		for (int i = 0; i < numeros.length; i++) {
-			numeros[i] = i + 1;
-			System.out.print(numeros[(int) Math.random () * 100] + " ");
+			//Con esto asignamos un numero aleatorio en cada posicion del array
+			numeros[i] = (int) Math.round(Math.random() * 100);
+			System.out.print(numeros[i] + " ");
+			if (i == 0) {
+				mayor=numeros[i];
+				menor=numeros[i];
+			}
+			if (numeros[i] > mayor && numeros[i]!=0) {
+				mayor=numeros[i];
+			}
+			if (numeros[i] < menor && numeros[i]!=0) {
+				menor=numeros[i];
+			}
+			suma = numeros[i] + suma;
+			
 		}
-
+		
+		System.out.println("La suma es " + suma);
+		System.out.println("El mayor es " + mayor);
+		System.out.println("El menor es " + menor);
 	}
 
 }
