287702188f36f77e5224729766b05d8230007f00
ruanox96 <72544074+ruanox96@users.noreply.github.com>
2020-12-14 14:18:51 +0100

    m

---
+++ src/examenObjetos/Baraja.java
@@ -0,0 +1,54 @@
+package examenObjetos;
+
+import capitulo04bloque01_PrimerosObjetos.Carta;
+
+public class Baraja {
+
+	Carta cartas[] = new Carta[52];
+
+	public Baraja() {
+		int id = 0;
+
+		cartas[0] = new Carta(1, "picas", 0);
+
+		for (int i = 0; i <= 13; i++) {
+			cartas[id] = new Carta(i, "picas", id);
+			id++;
+		}
+
+		for (int i = 0; i <= 13; i++) {
+			cartas[id] = new Carta(i, "diamantes", id);
+			id++;
+		}
+
+		for (int i = 0; i <= 13; i++) {
+			cartas[id] = new Carta(i, "tréboles", id);
+			id++;
+		}
+
+		for (int i = 0; i <= 13; i++) {
+			cartas[id] = new Carta(i, "corazones", id);
+			id++;
+		}
+
+	}
+
+	public void moverCartaDeAbajoHaciaArriba() {
+		Carta aux = cartas[0];
+		// Recorro las posiciones del array de primera a penúltima
+		for (int j = 0; j < cartas.length - 1; j++) {
+			cartas[j] = cartas[j + 1];
+		}
+		// La primera posición, guardada en numgaurdado, pasa a ser la ultima
+		cartas[cartas.length - 1] = aux;
+	}
+
+	public void moverCartaDeArribaHaciaAbajo() {
+		Carta aux = cartas[cartas.length - 1];
+		for (int j = cartas.length - 1; j > 0; j--) {
+			cartas[j] = cartas[j - 1];
+		}
+		cartas[0] = aux;
+	}
+
+}
--- src/examenObjetos/Carta.java
+++ src/examenObjetos/Carta.java
@@ -1,5 +1,64 @@
 package examenObjetos;
 
 public class Carta {
+	private int valor;
+	private String palo;
+	private int id;
+	
+	
+	
+	
+	public Carta() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Carta(int valor, String palo, int id) {
+		super();
+		this.valor = valor;
+		this.palo = palo;
+		this.id = id;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Carta [valor=" + valor + ", palo=" + palo + ", id=" + id + "]";
+	}
+
+
+	public int getValor() {
+		return valor;
+	}
+
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+
+	public String getPalo() {
+		return palo;
+	}
+
+
+	public void setPalo(String palo) {
+		this.palo = palo;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 
 }
+
+
+
