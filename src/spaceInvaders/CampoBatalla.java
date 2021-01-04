b0dc79b7918f78274baadae10bd426c47ed6ca5e
ruanox96 <72544074+ruanox96@users.noreply.github.com>
2021-01-04 13:20:48 +0100

    ss

--- src/spaceInvaders/CampoBatalla.java
+++ src/spaceInvaders/CampoBatalla.java
@@ -1,5 +1,8 @@
 package spaceInvaders;
 
+import capitulo04bloque01_PrimerosObjetos.Carta;
+import utils.Utils;
+
 public class CampoBatalla {
 
 	Humano humanos[] = new Humano[20];
@@ -22,6 +25,35 @@ public class CampoBatalla {
 				malvados[i].setPuntosVida(malvados[i].getPuntosVida()*2);
 			}
 		}
+	}
+	
+	
+	public void mezclarhumanos() {
+		for (int i = 0; i < 100; i++) {
+			int num1 = Utils.obtenerNumeroAzar(0, 51);
+			int num2 = Utils.obtenerNumeroAzar(0, 51);
+			
+			//intercambio de valores
+			Humano aux = humanos[num1];
+			humanos[num1] = humanos[num2];
+			humanos[num2] = aux;
+			
+		}
+		
+	}
+	
+	public void mezclarmalvados() {
+		for (int i = 0; i < 100; i++) {
+			int num1 = Utils.obtenerNumeroAzar(0, 51);
+			int num2 = Utils.obtenerNumeroAzar(0, 51);
+			
+			//intercambio de valores
+			Malvado aux = malvados[num1];
+			malvados[num1] = malvados[num2];
+			malvados[num2] = aux;
+			
+		}
+		
 	}
 
 }
--- src/spaceInvaders/Principal.java
+++ src/spaceInvaders/Principal.java
@@ -3,7 +3,7 @@ package spaceInvaders;
 public class Principal {
 
 	public static void main(String[] args) {
-		
+		CampoBatalla campobatalla = new CampoBatalla();
 
 	}
 
