0e2fa52d9bed31c3de1bea59ac0c985fd655adf2
ruanox96 <72544074+ruanox96@users.noreply.github.com>
2021-01-12 13:15:20 +0100

    ff

--- src/capitulo04_Herencia/ejerciciosSobreHerencia/articulosComestibles/Tienda.java
+++ src/capitulo04_Herencia/ejerciciosSobreHerencia/articulosComestibles/Tienda.java
@@ -5,8 +5,8 @@ public class Tienda {
 
 	public static void main(String[] args) {
 		
-		Perecederos p1 = new Perecederos(01, "Atun", 3, 21/05/21);
-		Perecederos p2 = new Perecederos(02, "Tomate", 2, 16/04/21);
+		Perecederos p1 = new Perecederos(01, "Atun", 3, "21/05/21");
+		Perecederos p2 = new Perecederos(02, "Tomate", 2, "16/04/21");
 
 	}
 
